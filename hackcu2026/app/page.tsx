cb4489682e31781b21ad9e689beb859c13796f10
Matveybu <bubalo.matvey@gmail.com>
2026-03-07 15:13:24 -0700

    description paragraph added

--- hackcu2026/app/page.tsx
+++ hackcu2026/app/page.tsx
@@ -1,19 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import {Button, Container, Stack, Title} from "@mantine/core";
+import {Box, Button, Container, Stack, Title} from "@mantine/core";
 import HomeTabs from "@/app/_components/home/tabs/HomeTabs";
 
 export default function Home() {
     return (
         <>
-            <Container size="md">
+            <Box p="xl">
                 <Stack>
                     <Title order={1}>Trade Truth</Title>
-
-
+                    In a world of of rugpulls and pump-and-dumps, TradeTruth stands for transparency. Our tool allows you to analyze various forms of financial advice to scan for potential red flags and allow you to avoid malicious actors.
                     <HomeTabs/>
                 </Stack>
-            </Container>
+            </Box>
         </>
     );
 }
